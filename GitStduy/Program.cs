71861d2be8178e028589dbb515744b41d17663e1
hegon910 <4poise@gmail.com>
2025-03-20 15:55:41 +0900

    DosomePractice

--- GitStduy/Program.cs
+++ GitStduy/Program.cs
@@ -1,5 +1,18 @@
-﻿namespace GitStduy
+﻿using System.Security.Cryptography;
+
+namespace GitStduy
 {
+    public struct StruExam
+    {
+        public string Name;
+        public int ID;
+        
+        public StruExam(string name, int howmany)
+        {
+            Name = name;
+            ID = howmany;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -8,6 +21,23 @@
             int iValue = 10;
             bValue = true || (++iValue > 5);
             Console.WriteLine(bValue);
+
+            int[] num= new int[3];
+            num[0] = 1;
+            num[1] = 2;
+            num[2] = 3;
+
+            Console.WriteLine("{0}{1}", num[1], num[2]);
+            string[] height = new string[5];
+            height[0] = "150";
+            height[1] = "151";
+            height[2] = "152";
+            height[3] = "153";
+            height[4] = "154";
+
+            Console.WriteLine("{0}\n{1}", height[1], height[2]);
+
+
         }
     }
 }
